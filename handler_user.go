e6256cd362d201a335ec98d8cba98cb88933cd48
Matthew Newman <theMatthewNewman@gmail.com>
2024-03-20 13:37:37 -0400

    updates

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/theMatthewNewman/scratch/internal/auth"
 	"github.com/theMatthewNewman/scratch/internal/database"
 )
 
@@ -31,5 +32,19 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
 		return
 	}
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("couldn't get user: %v", err))
+	}
+
 	respondWithJSON(w, 200, databaseUserToUser(user))
 }
